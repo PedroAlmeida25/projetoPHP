6a8b96a8ed336bda996ef0ef883f122b376a7a09
PEDRO AUGUSTO SANTOS ALMEIDA <pedro.asalmeida1@senacsp.edu.br>
2024-07-23 21:57:13 -0300

    fix: continuação

--- php/funcoes.php
+++ php/funcoes.php
@@ -1,8 +1,7 @@
 <?php
     $num1 = 0;
-    $num2 = 3;
-    $par = 0;
-    $impar = 0;
+    $num2 = 0;
+    $num3 = 0;
     $numero_binario = '1010';
     $numero_decimal = '32';
 
@@ -26,16 +25,93 @@
         }
     }//Fim do dividir
 
-    function binario($numero_binario){
-        $numero_decimal = decbin($numero_binario);
+    function converterBinario($decimal){
+        $binario = "";
+        while($decimal >= 1){
+            $binario .= $decimal % 2;
+            $decimal = $decimal / 2;
+        }
+        return strrev($binario);
     }
 
-    function decimal($numero_decimal){
-        $numero_binario = bindec($numero_decimal);
+    function converterDecimal($binario){
+        $tamanho = strlen($binario);
+        $j = $tamanho - 1;
+        $i = 0;
+        $decimal = 0;
+        while($i < $tamanho){
+            if(substr($binario,$i,1) == "1"){
+                $decimal += pow(2,$j);
+            }                                                                                                                
+            $j--;
+            $i++; 
+        }
+        return $decimal;
     }
 
     function parImpar($num1){
-    
+        if($num1 % 2 == 0){
+            return "par";
+        }else{
+            return "ímpar";
+        }
+    }
+
+    function idade($num1){
+        if($num1 < 18){
+            return "menor de idade";
+        }else if($num1 >= 18){
+            return "maior de idade";
+        }
+    }
+
+    function dobro($num1){
+        return $num1 * 2;
+    }
+
+    function converterFahrenheit($num1, $num2){
+        return $num1 = (9 * $num2 + 160) / 5;
+    }
+
+    function maiorTres($num1, $num2, $num3){
+        if(($num1 > $num2) && ($num1 > $num3)){
+            return $num1;
+        }else if(($num2 > $num1) && ($num2 > $num3)){
+            return $num2;
+        }else{
+            return $num3;
+        }
+    }
+
+    function positivoNegativo($num1){
+        if($num1 > 0){
+            return "positivo";
+        }else if($num1 = 0){
+            return "igual a zero";
+        }else if($num1 < 0){
+            return "menor que zero";
+        }
+    }
+
+    function tabuada($num1){
+        for ($i = 0; $i <= 10; $i++) { 
+
+            for ($contador = 0; $contador <= 10; $contador++){ 
+                while ($i == $contador) {
+                    echo "\n\nTabuada do $contador\n";
+                    break;
+                }
+            }
+
+            for ($num = 0; $num <= 10; $num ++){ 
+                $calc = $i * $num;
+                echo "$i X $num = $calc\n";
+            }
+        }
+    }
+
+    function media($num1, $num2, $num3){
+        return ($num1 + $num2 + $num3) / 3;
     }
 
     function escolher($num1){
@@ -60,6 +136,18 @@
                 break;
             case 7:
                 return "Laranja";
+                break;
+            case 8:
+                return "Verde";
+                break;
+            case 9:
+                return "Azul escuro";
+                break;
+            case 10:
+                return "Roxo";
+                break;
+            case 11:
+                return "Vinho";
                 break;
             default:
                 return "Cor não identificada";
--- php/index.php
+++ php/index.php
@@ -17,24 +17,36 @@
         <input type="number" id="num1" name="num1"><br>
 
         <label>Segundo número:</label>
-        <input type="number" id="num1" name="num2"><br>
+        <input type="number" id="num2" name="num2"><br>
+
+        <label>Terceiro número:</label>
+        <input type="number" id="num3" name="num3"><br>
 
         <button>Calcular
             <?php
                 $num1 = $_POST['num1'];
                 $num2 = $_POST['num2'];
+                $num3 = $_POST['num3'];
             ?>
         </button><br>
 
-        <textarea cols="40" rows="100" readOnly>
+        <textarea cols="100" rows="100" readOnly>
             <?php 
-                echo somar($num1, $num2).
-                    "\n".subtrair($num1, $num2).
-                    "\n".mult($num1, $num2).
-                    "\n".dividir($num1, $num2).
-                    "\n".escolher($num1).
-                    "\n".decbin($num2).
-                    "\n".bindec($num1);
+               echo "\nA soma é: ".somar($num1, $num2).
+                    "\nA subtração é: ".subtrair($num1, $num2).
+                    "\nA multiplicação é: ".mult($num1, $num2).
+                    "\nA divisão é: ".dividir($num1, $num2).
+                    "\nA cor escolhida é: ".escolher($num1).
+                    "\nA transferência de decimal para binário é: ".converterBinario($numero_binario).
+                    "\nA transferência de binário para decimal é: ".converterDecimal($numero_decimal).
+                    "\nEsse número é: ".parImpar($num1).
+                    "\nVocê é: ".idade($num1).
+                    "\nO dobro desse número é: ".dobro($num1).
+                    "\nA conversão de celsius para fahrenheit é: ".converterfahrenheit($num1, $num2).
+                    "\nO maior número é: ".maiorTres($num1, $num2, $num3).
+                    "\nO número é: ".positivoNegativo($num1).
+                    "\n".tabuada($num1).
+                    "\nA média dos três números é: ".media($num1, $num2, $num3);
             ?>
         </textarea>
     </form>
